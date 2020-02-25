10cb2ac8fd8f7dff90524b6badfcb447791f9346
seden0957 <seden0957@7KTM6Y1.Student.UGDSB.ED>
2020-02-25 13:58:41 -0500

    bruh

---
+++ Palindrome.java/src/palindrome/java/PalindromeJava.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package palindrome.java;
+import java.util.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author seden0957
+ */
+public class PalindromeJava {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String str = JOptionPane.showInputDialog("Input believed Palindrome:");
+    
+        for(int i = 0; i < str.length(); i++) {
+    }
+    
+}
+}
